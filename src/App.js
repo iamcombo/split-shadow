ffc58a9d10ba6b9651909d07ef4c3fa442a5d73a
Piset Heang <piset2cambo@gmail.com>
2020-10-12 14:05:55 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <h1>U</h1>
+      </section>
+      <main>
+        <h1>X</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
